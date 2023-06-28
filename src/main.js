451427efae537ed8668807e108f77565baa73d25
Doug Hahn <doughahn@gmail.com>
2023-06-27 23:33:13 -0700

    rm old fonts
    Try to jank my way to avopid render bloicking hoefler

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
 import DefaultLayout from '~/layouts/Default.vue'
 import Figure from '~/components/Figure.vue'
 import '~/styles/main.scss'
@@ -10,9 +7,20 @@ export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
   Vue.component('Figure', Figure)
-  // add external css
-  head.link.push({
-    rel: 'stylesheet',
-    href: 'https://cloud.typography.com/7216232/7655832/css/fonts.css'
-  })
+
+  if (process.isClient) {
+    // Add the script on the client-side via Vue's mounted lifecycle hook
+    Vue.mixin({
+      mounted() {
+        const link = document.createElement('link')
+        link.rel = 'stylesheet'
+        link.href = 'https://cloud.typography.com/7216232/7655832/css/fonts.css'
+        link.media = 'print'
+        link.onload = function() {
+          this.media = 'all'
+        }
+        document.head.appendChild(link)
+      },
+    })
+  }
 }
