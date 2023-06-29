b388713ffcebdda520865fa8135ad588912aade2
Doug Hahn <doughahn@gmail.com>
2023-06-29 08:46:39 -0700

    curly quotes

--- src/components/AboutAside.vue
+++ src/components/AboutAside.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
       <h2>About Me</h2>
-      <h3>I'm a Project Person</h3>
+      <h3>I&rsquo;m a Project Person</h3>
       <p class="leadin">
-        <b>I'm a PMP</b> with an MFA in poetry and deep technical ability. As an experienced generalist I command expertise in a variety of domains. 
+        <b>I&rsquo;m a PMP</b> with an MFA in poetry and deep technical ability. As an experienced generalist I command expertise in a variety of key domains.
       </p>
       <h4 class="leadin-subhead">Three things mean everything to me:</h4>
       <ul class="leadin-list">
