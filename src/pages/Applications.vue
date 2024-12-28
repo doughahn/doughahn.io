1827e1f0819f1bfd9304ce978f17fe46c3a1c061
Doug Hahn <doughahn@gmail.com>
2024-12-28 15:33:32 -0800

    LLNL

---
+++ src/pages/Applications.vue
@@ -0,0 +1,59 @@
+<template>
+    <Layout :key="$route.fullPath">
+        <transition name="fade" appear>
+          <main>
+      <section class="article-grid"> 
+        <article>
+          <h2>Applications</h2>
+          <h3>Interested in hiring me?</h3> 
+          <p>Find me on <a href="https://www.linkedin.com/in/douglas-hahn/">LinkedIn</a>!</p>
+      </article>
+      </section>
+    </main>
+    </transition>
+    </Layout>
+  </template>
+  
+  <script>
+  import TaggedPosts from '@/components/TaggedPosts.vue';
+  import AboutAside from '@/components/AboutAside.vue';
+
+  export default {
+    name: 'WorkExperience',
+    components: {
+      TaggedPosts,
+      AboutAside
+    },
+    metaInfo() {
+    return {
+      title: 'Doug Hahn\'s Applications',
+      meta: [
+        {
+          name: 'description',
+          content: 'I\'m a skilled generalist, with projects spanning a variety of domains — including technical project management, strategy, to technical writing, and web development.'
+        },
+        {
+          name: 'keywords',
+          content: 'project management, collaboration, communication, content strategy, technical writing, technical editing, web development, app development, marketing, writing, editing, documentation, PMP',
+        }
+      ]
+    }
+  },
+  }
+  </script>
+  
+  
+  <page-query>
+
+    </page-query>
+  
+  <style>
+  .fade-enter-active {
+    transition: opacity .5s;
+  }
+  
+  .fade-enter {
+    opacity: 0;
+  }
+  </style>
+  
---
+++ src/templates/Applicant.vue
@@ -0,0 +1,37 @@
+<template>
+  <Layout :key="$route.fullPath">
+    <main>
+          <transition name="fade" appear>
+            <div v-html="$page.applicant.content"></div>
+          </transition>
+    </main>
+  </Layout>
+</template>
+
+<script>
+import { MetaInfo } from 'vue-meta';
+import AboutAside from '@/components/AboutAside.vue';
+import Layout from '~/layouts/Custom.vue'
+
+
+export default {
+  components: {
+      Layout
+    },
+  metaInfo() {
+    return {
+      title: this.$page.applicant.title,
+      meta: [
+        {
+          name: 'description',
+          content: this.$page.applicant.summary,
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<page-query>
+
+</page-query>
